e4d3285008e6ace2aa476ed8cec2371dccf71f53
bigboiberke <95446280+bigboiberke@users.noreply.github.com>
2022-02-17 11:41:14 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+  using namespace std;
 
-int main() {
-    std::cout << "Hello, world!\n";
+int divisorSum(int n)
+{
+    int sum = 0;
+
+    for(int i = 1; i <= n; ++i)
+    {
+
+        for(int j = 1; j * j <= i; ++j)
+        {
+            if (i % j == 0)
+            {
+                if (i / j == j)
+                    sum += j;
+                else
+                    sum += j + i / j;
+            }
+        }
+    }
+    return sum;
+}
+
+
+int main()
+{
+    int n = 4;
+    cout << " " << divisorSum(n) << endl;
+
+    n = 5;
+    cout << " " << divisorSum(n);
+
     return 0;
 }
